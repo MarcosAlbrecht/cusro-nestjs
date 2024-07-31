e3750704798b80494beb9464b620e3a14731fdc8
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2024-07-31 09:08:45 -0300

    create cache service and controller

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,15 +1,17 @@
-import { CacheModule } from '@nestjs/cache-manager';
+import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from 'src/cache/cache.module';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { CityEntity } from './entities/city.entity';
 
 @Module({
   imports: [
-    CacheModule.register({
+    CacheModuleNest.register({
       ttl: 900000000,
     }),
+    CacheModule,
     TypeOrmModule.forFeature([CityEntity]),
   ],
   controllers: [CityController],
